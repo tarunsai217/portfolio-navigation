c7818bf1c5e8c7d1ec47a24a56df1388494de0cc
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-15 13:59:28 +0530

    added more skills data

---
+++ Components/Skills/Assets/index.js
@@ -0,0 +1,6 @@
+import html5 from './html5.svg';
+import css3 from './css3.svg';
+import sass from './sass.svg';
+import js from './javascript.svg';
+
+export { html5, css3, js, sass };
--- Components/Skills/Skills.jsx
+++ Components/Skills/Skills.jsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import './Skills.scss';
 import SkillItem from './Components/SkillItem';
-import html5 from './Assets/html5.svg';
+import { html5, css3, js, sass } from './Assets';
+
 export default function Skills() {
-  const skillItems = [{ name: 'HTML 5', img: html5 }];
+  const skillItems = [
+    { name: 'HTML 5', img: html5 },
+    { name: 'CSS 3', img: css3 },
+    { name: 'JS', img: js },
+    { name: 'sass', img: sass },
+  ];
   return (
     <div>
       <h1>Skills</h1>
