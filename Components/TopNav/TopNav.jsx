b98d0e5c3a22cbc07493f6e8ab1c06a290f9ec3d
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-15 13:19:32 +0530

    feat - top navigation stylying

--- Components/TopNav/TopNav.jsx
+++ Components/TopNav/TopNav.jsx
@@ -4,11 +4,15 @@ import NavItem from './Components/NavItem';
 export default function TopNav() {
   const navItems = [{ name: 'HOME', link: 'home' }];
   return (
-    <div>
-      <h1>TopNav</h1>
-      {navItems.map((item, index) => {
-        return <NavItem item={item} key={index} />;
-      })}
+    <div className="top-navigation">
+      <div className="logo-wrapper">
+        <img src="https://i.ibb.co/hL4ZLk8/logo.png" alt="logo" />
+      </div>
+      <ul className="nav-items">
+        {navItems.map((item, index) => {
+          return <NavItem item={item} key={index} />;
+        })}
+      </ul>
     </div>
   );
 }
