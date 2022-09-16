f207ed1239e2856471d4e80add9d5b7436498d6d
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-16 07:17:21 +0530

    bug - UI enhanced

--- Components/Projects/Projects.jsx
+++ Components/Projects/Projects.jsx
@@ -12,13 +12,51 @@ export default function Projects() {
       img: 'https://i.ibb.co/ns4QR7d/sample-image.png',
       skills: ['VUE3', 'API', 'VITE'],
     },
+    {
+      name: 'Anime Tracker',
+      description:
+        "An Anime tracker app where you can track the anime episodes you're currently watching. Made using Vue3",
+      gitLink: '',
+      urlLink: '',
+      img: 'https://i.ibb.co/ns4QR7d/sample-image.png',
+      skills: ['VUE3', 'API', 'VITE'],
+    },
+    {
+      name: 'Anime Tracker',
+      description:
+        "An Anime tracker app where you can track the anime episodes you're currently watching. Made using Vue3",
+      gitLink: '',
+      urlLink: '',
+      img: 'https://i.ibb.co/ns4QR7d/sample-image.png',
+      skills: ['VUE3', 'API', 'VITE'],
+    },
+    {
+      name: 'Anime Tracker',
+      description:
+        "An Anime tracker app where you can track the anime episodes you're currently watching. Made using Vue3",
+      gitLink: '',
+      urlLink: '',
+      img: 'https://i.ibb.co/ns4QR7d/sample-image.png',
+      skills: ['VUE3', 'API', 'VITE'],
+    },
+    {
+      name: 'Anime Tracker',
+      description:
+        "An Anime tracker app where you can track the anime episodes you're currently watching. Made using Vue3",
+      gitLink: '',
+      urlLink: '',
+      img: 'https://i.ibb.co/ns4QR7d/sample-image.png',
+      skills: ['VUE3', 'API', 'VITE'],
+    },
   ];
   return (
-    <div>
-      <h1>Projects</h1>
-      {projectItems.map((item, index) => {
-        return <ProjectItem item={item} key={index} />;
-      })}
+    <div className="projects-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-items">
+        {projectItems.map((item, index) => {
+          return <ProjectItem item={item} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
