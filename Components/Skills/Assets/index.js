a2389ea4624897823a19795b48d097068661c8d0
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-16 05:57:58 +0530

    bug -skills stylying enhanced

--- Components/Skills/Assets/index.js
+++ Components/Skills/Assets/index.js
@@ -2,5 +2,9 @@ import html5 from './html5.svg';
 import css3 from './css3.svg';
 import sass from './sass.svg';
 import js from './javascript.svg';
+import react from './react.svg';
+import redux from './redux.svg';
+import tailwind from './tailwind.svg';
+import bootstrap from './bootstrap.svg';
 
-export { html5, css3, js, sass };
+export { html5, css3, js, sass, react, redux, tailwind, bootstrap };
--- Components/Skills/Skills.jsx
+++ Components/Skills/Skills.jsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
 import './Skills.scss';
 import SkillItem from './Components/SkillItem';
-import { html5, css3, js, sass } from './Assets';
+import {
+  html5,
+  css3,
+  js,
+  sass,
+  react,
+  redux,
+  tailwind,
+  bootstrap,
+} from './Assets';
 
 export default function Skills() {
   const skillItems = [
@@ -9,13 +18,23 @@ export default function Skills() {
     { name: 'CSS 3', img: css3 },
     { name: 'JS', img: js },
     { name: 'sass', img: sass },
+    { name: 'react', img: react },
+    { name: 'redux', img: redux },
+    { name: 'tailwind', img: tailwind },
+    { name: 'bootstrap', img: bootstrap },
+    { name: 'node js', img: sass },
+    { name: 'express', img: sass },
+    { name: 'firebase', img: sass },
+    { name: 'socket io', img: sass },
   ];
   return (
-    <div>
-      <h1>Skills</h1>
-      {skillItems.map((item, index) => {
-        return <SkillItem item={item} key={index} />;
-      })}
+    <div className="skills-container">
+      <h1 className="skill-heading">Skills</h1>
+      <div className="skill-items">
+        {skillItems.map((item, index) => {
+          return <SkillItem item={item} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
